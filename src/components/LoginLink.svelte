2cce387b04abf701c972f08984f0a8da3cecd07d
Steve Watts <stevewatts@MacBook-Pro.local>
2021-01-03 16:21:05 -0800

    First Committ

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,17 @@
+<script>
+    import user from '../stores/user';
+    import {link} from 'svelte-routing';
+    //logout functionality
+    import globalStore from '../stores/globalStore';
+</script>
+
+
+{#if $user.jwt}
+    <a href="/" class="logout-btn" on:click={() => {
+        globalStore.toggleItem('sidebar', false)
+    }}>logout</a>
+{:else}
+<a href="/login" class="logout-btn" on:click={() => {
+    globalStore.toggleItem('sidebar', false)
+}}>login</a>
+{/if}
